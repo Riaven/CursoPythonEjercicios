87d0a3ec60e3dd0e2658824490648c649f661e6b
Riaven <jogubaez@hotmail.com>
2019-07-23 19:14:40 -0400

    declaración de función y llamada de la misma

---
+++ primeras_funciones.py
@@ -0,0 +1,9 @@
+#A continuación se explica  como utilizar funciones propias#
+#Declaración de la función
+def mensaje():
+    #cuerpo de la función
+    print("git init")
+    print("git add .")
+    print("git status")
+#llamada a la función
+mensaje()
