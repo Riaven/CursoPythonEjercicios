056459151e9d1ca2e01949174a2bfa55c1363c03
Riaven <jogubaez@hotmail.com>
2019-07-23 16:37:40 -0400

    Ejmplo de uso de variables y utilización

---
+++ ejercicio1.py
@@ -0,0 +1,14 @@
+nombre = "I am Iron Man" #esto le da el tipo de dato que
+                         #que es la variable
+print(nombre) #imprime lo que contiene nombre
+print(type(nombre))  # imprime el tipo de dato que es la variable
+
+
+#En este caso la variable nombre, su tipo es definido por
+# el contenido que a esta se le da, en el caso anterior se
+# le entrego un contenido de tipo string, y ahora uno int
+# y en la función type() se puede observar que cambio de ser 
+# str a int#
+nombre = 5
+print(nombre)
+print(type(nombre))
