6a8ca9f69ef7184359e3ac0c5e98acf50f7ebfa2
Riaven <jogubaez@hotmail.com>
2019-07-23 20:40:35 -0400

    Listas

---
+++ practica listas.py
@@ -0,0 +1,12 @@
+#A continuación se creará una lista para almacenar datos
+# y posteriormente hacer un llamado a la lista completa, como
+# a un solo dato por su índice#
+
+miLista = ["Osiris", "Sylvia", "Carla", "Adriadna"]
+#Se ponen dos puntos dentro de los corchetes para poder
+# obtener todos los datos que contiene la lista llamada#
+print(miLista[:])
+#En el caso de querer obtener un dato en concreto de una lista
+# al momento de imprimir, dentro de los corchetes se le pasa
+# el índice del elemento en cuestión#
+print(miLista[0])
