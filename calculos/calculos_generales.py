10ce913a9aea3a9c32923980573f904c75b430af
Riaven <jogubaez@hotmail.com>
2019-08-03 23:28:24 -0400

    Uso de paquetes

---
+++ calculos/calculos_generales.py
@@ -0,0 +1,17 @@
+def sumar(num1, num2):
+    print("El resultado de la suma es :", num1 + num2)
+
+def restar(num1, num2):
+    print("El resultado de la resta es :", num1 - num2)
+
+def multiplicar(num1, num2):
+    print("El resultado de la multiplicación es :", num1 * num2)
+
+def dividir(num1, num2):
+    print("El resultado de la división es :", num1 / num2)
+
+def potencia(base, exponente):
+    print("El resultado de la suma es :", base**exponente)
+
+def redondear(numero):
+    print("El resultado del redondeo es : ", round(numero))
