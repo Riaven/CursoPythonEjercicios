024dbbd1218302ccf29a0e428e08b22d428bb711
Riaven <jogubaez@hotmail.com>
2019-09-20 19:46:09 -0300

    Manejo de archivos con append (modificar)

--- manejo_archivos.py
+++ manejo_archivos.py
@@ -10,7 +10,19 @@ print("Aquí se ha escrito en el archivo")
 archivo_texto = open("miArchivo.txt", "r") # read
 # le otorga a la variable .texto. el contenido del archivo que se abrió en modo lectura anteriormente
 texto = archivo_texto.read() 
+
+#texto = archivo_texto.readline() <-- lee línea por línea, lo almacena en una lista
+#texto[1] <-.primera línea
 #Siempre una vez utilizado el archivo, se debe de cerrar, para no ocupar memoria
 archivo_texto.close()
 print("Se ha leído desde el archivo")
 print(texto) # imprime lo que se almacenó en la variable
+
+
+#abrir el archivo en modo extención o en modo agregar
+
+archivo_texto = open("miArchivo.txt", "a") # <-- append
+#se utiliza el archivo junto con el método write
+archivo_texto.write(" \n siempre se puede agregar algo adicional :)")
+archivo_texto.close()
+
