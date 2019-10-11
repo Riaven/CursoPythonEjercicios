5f8c1ca11edc0a8f2696bda49c6c5bae0c269762
Riaven <jogubaez@hotmail.com>
2019-10-11 17:29:17 -0300

    fixed

--- interfaces_Graficas/primera_interface.py
+++ interfaces_Graficas/primera_interface.py
@@ -9,5 +9,4 @@ raiz.resizable(0,0) #width, heigth ---- permite boolean
 # La ventana pra permanecer abierta, debe de estar a la escucha de cualquier eventro
 # eso se logra con el método mainloop(), permanece en un loop infinito
 #Siempre este método debe de ir al último del todo el código
-
 raiz.mainloop()
