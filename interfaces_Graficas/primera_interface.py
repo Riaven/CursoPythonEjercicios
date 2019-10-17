b0326a88a296d521c1499ed18b0ce7e7f05a87f1
Riaven <jogubaez@hotmail.com>
2019-10-17 19:39:06 -0300

    Añadiendo frame

--- interfaces_Graficas/primera_interface.py
+++ interfaces_Graficas/primera_interface.py
@@ -5,15 +5,23 @@ raiz=Tk()
 #Titulo de la ventana
 raiz.title("Ventana")
 #no permite cambiar el tamño de la ventana
-raiz.resizable(1,0) #width, heigth ---- permite boolean
+#raiz.resizable(1,0) #width, heigth ---- permite boolean
 #Para poder cambiar el ícono de la ventana, se debe de utilizar una imagen .ico
 raiz.iconbitmap("gato.ico")
 
 #Ancho y alto de la ventana
-raiz.geometry("450x500")
-
+#La raiz siempre se va a adaptar a su contenedor interno, darle tamaño a la raiz no es válido
+#raiz.geometry("450x500")
+#Cambia el color del fondo de la ventana
 raiz.config(bg="green")
 
+#Crear un Frame
+miFrame = Frame()
+#empequeta el frame dentro de la ventana
+miFrame.pack()
+#Le da un color al frame
+miFrame.config(bg="blue")
+miFrame.config(width = "650", height = "350")
 
 # La ventana pra permanecer abierta, debe de estar a la escucha de cualquier eventro
 # eso se logra con el método mainloop(), permanece en un loop infinito
