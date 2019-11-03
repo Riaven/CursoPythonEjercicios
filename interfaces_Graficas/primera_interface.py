e4983eb24a299061ef41d319fc50cc29de02aca0
Riaven <jogubaez@hotmail.com>
2019-11-02 23:50:50 -0300

    Frames configuracion y características

--- interfaces_Graficas/primera_interface.py
+++ interfaces_Graficas/primera_interface.py
@@ -18,11 +18,24 @@ raiz.config(bg="green")
 #Crear un Frame
 miFrame = Frame()
 #empequeta el frame dentro de la ventana
+#side permite = left, right, top, bottom
+#anchor permite = n , s, w, e <-- puntos cardinales
+#fill efecto de rellenado
+
 miFrame.pack()
 #Le da un color al frame
 miFrame.config(bg="blue")
 miFrame.config(width = "650", height = "350")
 
+#Tamaño del borde del frame
+miFrame.config(bd=35)
+#Tipo de borde
+miFrame.config(relief= "groove")
+
+
+#Para cambiar cursor
+
+miFrame.config(cursor="pirate")
 # La ventana pra permanecer abierta, debe de estar a la escucha de cualquier eventro
 # eso se logra con el método mainloop(), permanece en un loop infinito
 #Siempre este método debe de ir al último del todo el código
