3c8b7a0f4472909774758cdd9a52ee99ecdf7234
Riaven <jogubaez@hotmail.com>
2020-06-20 17:07:23 -0400

    cambio en lambda

---
+++ funciones _lambda.py
@@ -0,0 +1,8 @@
+# Ejercicios para practicar lambda, son como las funciones anónimas que posee Javascript#
+# Esto sería una función normal
+def funcion(base, altura):
+    return base/altura
+
+print(funcion(23, 3))
+
+            
