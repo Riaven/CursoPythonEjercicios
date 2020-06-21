48d2fa03e2cfed64546bf94f812c37d6117d4cbb
Riaven <jogubaez@hotmail.com>
2020-06-20 23:10:00 -0400

    Lambda

--- funciones _lambda.py
+++ funciones _lambda.py
@@ -1,8 +1,12 @@
 # Ejercicios para practicar lambda, son como las funciones anónimas que posee Javascript#
 # Esto sería una función normal
 def funcion(base, altura):
-    return base/altura
-
+    return (base*altura)/2
+#Usando la función anterior
 print(funcion(23, 3))
 
-            
+area_triangulo = lambda base, altura : (base*altura)/2
+print(area_triangulo(23,3))
+
+#llamadas de igual manera como on demand, on line son funciones que usualmente 
+# si fueran funciones normales, solo usarían una línea de código#
